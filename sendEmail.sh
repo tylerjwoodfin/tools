9930720da65b066734cadcb958d8e153412c9b3d
Tyler Woodfin <tylerjwoodfin@gmail.com>
2020-06-19 22:05:06 -0500

    Changed emails to be sent from sendEmail.sh for easier maintenance and formatting

---
+++ sendEmail.sh
@@ -0,0 +1,30 @@
+# ReadMe
+# Sends emails using msmtp
+# This script is used to avoid having to pass parameters every time
+# Usage: bash sendEmail.sh <to> <subject> <message in HTML format> <sender name (optional)>
+#!/bin/bash
+
+# Assign variables
+to=$1
+subject=$2
+message=$3
+sender=$4
+
+if [ -z "$4" ]; then
+	sender="Raspberry Pi"
+fi
+
+if echo $message | mail \
+-a "From: $sender" \
+-a "MIME-Version: 1.0" \
+-a "Content-Type: text/html" \
+-s "$subject" \
+$to; then
+	echo "Sent Email"
+#	echo "to: $to"
+#	echo "subject: $subject"
+#	echo "message: $message"
+#	echo "from: $sender"
+else
+	echo; echo "Usage: bash sendEmail.sh <to> <subject> <message in HTML format> <sender name (optional)>"
+fi
