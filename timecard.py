f01cec9a12f50bbab8501af8160d8fdab80d8129
Your Name Here (In Lights) <tyler@tylerwoodfin.com>
2020-06-25 18:10:02 -0500

    Fixed timecard email not sending due to relative path

--- timecard.py
+++ timecard.py
@@ -15,4 +15,4 @@ for i in contacts:
     message = "Hi " + i.split(":")[1] + ",<br><br>Please remember to enter your time.<br><br>Thanks,<br><br>- " + sentFrom + "<br><br>Reply STOP to unsubscribe"
     
     # send email <to> <subject> <message> <from (optional)>
-    os.system("bash sendEmail.sh " + i.split(":")[2] + " \"Timecard Reminder\" \"" + message + "\" " + "\"" + sentFrom + "\"")
+    os.system("bash /home/pi/Tools/sendEmail.sh " + i.split(":")[2] + " \"Timecard Reminder\" \"" + message + "\" " + "\"" + sentFrom + "\"")
