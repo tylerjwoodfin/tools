f3032b00eb1f0e6b0341eea07432258cfb6fff53
Tyler Woodfin <tylerjwoodfin@gmail.com>
2021-01-30 22:38:26 -0600

    Updated Documentation

--- ping.py
+++
@@ -1,9 +0,0 @@
-# ReadMe
-# A function to test whether my phone is on my network (192.168.1.109)
-#!/usr/bin/env python3
-import subprocess
-from subprocess import DEVNULL, STDOUT, check_call, os
-
-pingCode = subprocess.Popen(["ping","-c","1","192.168.1.109"], stdout=DEVNULL, stderr=subprocess.STDOUT)
-pingCode.communicate()[0]
-print("Phone Connected: " + str(1-pingCode.returncode))
