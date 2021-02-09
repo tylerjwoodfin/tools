ed4c67b3e9c0c12568012f19cafb42b08afdfe46
Your Name Here (In Lights) <tyler@tylerwoodfin.com>
2021-02-08 22:56:36 -0600

    Improved GoogleHomeTest

---
+++ GoogleHomeTest/ReadMe.md
@@ -0,0 +1,7 @@
+# Description
+
+A simple test to control my Google Home Mini in my upstairs office with my Raspberry Pi.
+
+# Usage: 
+- node index.js to run
+- curl localhost:3000/test to test
--- timecard.py
+++ timecard.py
@@ -12,7 +12,7 @@ for i in contacts:
     if(i.split(":")[0] == "hub"):
         sentFrom = "Oracle Austin Hub Team"
 
-    message = "Hi " + i.split(":")[1] + ",<br><br>Please remember to enter your time.<br><br>Thanks,<br><br>- " + sentFrom + "<br><br>Reply STOP to unsubscribe"
+    message = "Hi " + i.split(":")[1] + ",<br><br>Please remember to enter your time. Automatic time card emails from this system will be unavailable after February 19. Please consider using <a href='https://support.microsoft.com/en-us/office/set-or-remove-reminders-7a992377-ca93-4ddd-a711-851ef3597925'>reminders in Outlook</a>.<br><br>Thanks,<br><br>- " + sentFrom + "<br><br>Reply STOP to unsubscribe"
     
     # send email <to> <subject> <message> <from (optional)>
-    os.system("bash /home/pi/Tools/sendEmail.sh " + i.split(":")[2] + " \"Timecard Reminder\" \"" + message + "\" " + "\"" + sentFrom + "\"")
+    os.system("bash /home/pi/Tools/sendEmail.sh " + i.split(":")[2] + " \"Timecard Reminder Deprecation Notice\" \"" + message + "\" " + "\"" + sentFrom + "\"")
