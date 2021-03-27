9a766872b98f49f1455f46875337ac3207e1a3f9
Tyler Woodfin <tylerjwoodfin@gmail.com>
2021-03-26 23:04:00 -0500

    Updated nest.py for easier token renewals, better readability

--- nest.py
+++ nest.py
@@ -1,48 +1,47 @@
 # ReadMe
 # Pulls the current humidity and sends an email to turn humidifier on if humidity is low
-# You need a Google Device Access Console account. Instructions for registering as of 23 JAN 2021: https://www.wouternieuwerth.nl/controlling-a-google-nest-thermostat-with-python/
+# You need a Google Device Access Console account.
+# Reference as of 23 JAN 2021: https://www.wouternieuwerth.nl/controlling-a-google-nest-thermostat-with-python/
 # Make sure redirect_uri is the same in this file as in the Google Device Access Console account.
 
 import requests
 import secureData
 import os
 
-# create 'SecureData/NestIDs' and place these variables in the first 4 lines
-project_id = secureData.array("NestIDs")[0]
-client_id = secureData.array("NestIDs")[1]
-client_secret = secureData.array("NestIDs")[2]
-redirect_uri = 'https://www.tyler.cloud'
-code = secureData.array("NestIDs")[3]
-
-url = 'https://nestservices.google.com/partnerconnections/'+project_id+'/auth?redirect_uri='+redirect_uri+'&access_type=offline&prompt=consent&client_id='+client_id+'&response_type=code&scope=https://www.googleapis.com/auth/sdm.service'
+# Use only to log in manually
+# url = 'https://nestservices.google.com/partnerconnections/'+project_id+'/auth?redirect_uri='+redirect_uri+'&access_type=offline&prompt=consent&client_id='+client_id+'&response_type=code&scope=https://www.googleapis.com/auth/sdm.service'
 # print("Go to this URL to log in:")
 # print(url)
 
-params = (
-    ('client_id', client_id),
-    ('client_secret', client_secret),
-    ('code', code),
-    ('grant_type', 'authorization_code'),
-    ('redirect_uri', redirect_uri),
-)
+# Constants
+project_id = secureData.array("NestIDs")[0]
+client_id = secureData.array("NestIDs")[1]
+client_secret = secureData.array("NestIDs")[2]
+code = secureData.array("NestIDs")[3]
+redirect_uri = 'https://www.tyler.cloud'
 
+# Access token- do we need to refresh?
 access_token = secureData.variable("NestAccessToken")
-refresh_token = secureData.variable("NestRefreshToken")
 
-params = (
-    ('client_id', client_id),
-    ('client_secret', client_secret),
-    ('refresh_token', refresh_token),
-    ('grant_type', 'refresh_token'),
-)
+# Get new Access Token by Passing the Refresh Token
+def renewAccessToken():
+    params = (
+        ('client_id', client_id),
+        ('client_secret', client_secret),
+        ('refresh_token', secureData.variable("NestRefreshToken")),
+        ('grant_type', 'refresh_token')
+    )
 
-response = requests.post('https://www.googleapis.com/oauth2/v4/token', params=params)
+    response = requests.post('https://www.googleapis.com/oauth2/v4/token', params=params)
 
-response_json = response.json()
-print(response.json())
-access_token = response_json['token_type'] + ' ' + response_json['access_token']
-secureData.write("NestAccessToken", access_token)
-print('Access token: ' + access_token)
+    response_json = response.json()
+
+    print(response.json())
+
+    access_token = response_json['token_type'] + ' ' + response_json['access_token']
+    print('Access token: ' + access_token)
+      
+    secureData.write("NestAccessToken", access_token)
 
 # Get structures
 
@@ -55,7 +54,7 @@ headers = {
 
 response = requests.get(url_structures, headers=headers)
 
-print(response.json())
+# print(response.json())
 
 # Get devices
 
@@ -68,11 +67,10 @@ headers = {
 
 response = requests.get(url_get_devices, headers=headers)
 
-print(response.json())
+# print(response.json())
 
 response_json = response.json()
 device_0_name = response_json['devices'][0]['name']
-print(device_0_name)
 
 # Get device stats
 
@@ -91,9 +89,17 @@ print('Humidity:', humidity)
 temperature = response_json['traits']['sdm.devices.traits.Temperature']['ambientTemperatureCelsius']
 print('Temperature:', temperature)
 
-if(humidity < 35):
+renewAccessToken()
+
+if(humidity > 35):
     # Email Variables
     sentFrom = "Raspberry Pi"
     email = secureData.variable("email")
-    message = "Hi Tyler,<br><br>Your humidity at home is " + str(humidity) + "%. I recommend turning on your humidifier.<br><br>Thanks,<br><br>- " + sentFrom
+    message = """\
+    Hi Tyler,<br><br>
+    Your humidity at home is %s. I recommend turning on your humidifier.
+    <br><br>Thanks,
+    <br><br>- %s"""
+    
+    message = message % (str(humidity) + "%",sentFrom)
     os.system("bash /home/pi/Git/Tools/sendEmail.sh " + email + " \"Low Humidity\" \"" + message + "\" " + "\"" + sentFrom + "\"")
