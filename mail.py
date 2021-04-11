15c93dbafb9e15180fa0aa16466db30c2b63c4e8
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-04-10 22:13:50 -0500

    Added more documentation to mail.py; backing up ~/.bashrc to my website

--- mail.py
+++ mail.py
@@ -6,18 +6,27 @@ import secureData
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# ReadMe
+# This is a simple module to send mail through smtplib.
+# For Gmail, you will need to enable "less secure apps".
+# 
+# This is called from a wrapper function in /usr/sbin/rmail. 
+# I'm using unquote (a.k.a. URL decode) for each parameter in case this is being called from web servers or from other
+# scripts with the need to escape quotation characters.
+
 # Parameters
 port = 465
 smtp_server = "smtp.gmail.com"
+username = secureData.variable("email_pi")
 password = secureData.variable("email_pi_pw")
 
 def send(subject, body, signature="<br><br>Thanks,<br>Raspberry Pi", to=secureData.variable("email")):    
     
     # Parse
-    body += signature
+    body += unquote(signature)
     message = MIMEMultipart()
-    message["Subject"] = subject
-    message["From"] = secureData.variable("email_pi")
+    message["Subject"] = unquote(subject)
+    message["From"] = "Raspberry Pi <" + username + ">"
     message["To"] = secureData.variable("email")
     message.attach(MIMEText(unquote(body), "html"))
 
@@ -25,10 +34,13 @@ def send(subject, body, signature="<br><br>Thanks,<br>Raspberry Pi", to=secureDa
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(message["From"], password)
+        server.login(username, password)
         server.sendmail(message["From"], message["To"], message.as_string())
         print("Sent Email")
    
 # By default, mail.send:     
 if(len(sys.argv) == 3):
 	send(sys.argv[1], sys.argv[2])
+
+if(len(sys.argv) == 1):
+    print("Usage: sendMail <subject>, <body>, <signature>, <to_email>")
