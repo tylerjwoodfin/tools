34ccf4412246cffde6f7d032c9ffd3f40bd63779
Tyler Woodfin <tyler@tylerwoodfin.com>
2021-10-10 23:01:16 -0500

    Added missing import; improved formatting

--- weatherAlerts.py
+++ weatherAlerts.py
@@ -11,8 +11,9 @@ from decimal import Decimal as d
 import random
 import sys
 import pwd
+import os
 
-userDir = pwd.getpwuid( os.getuid() )[ 0 ]
+userDir = pwd.getpwuid(os.getuid())[0]
 
 sys.path.insert(0, f'/home/{userDir}/Git/SecureData')
 import secureData
@@ -20,10 +21,13 @@ import secureData
 lat = str(secureData.array("weatherLatLong")[0])
 lon = str(secureData.array("weatherLatLong")[1])
 
+
 def datetime_from_utc_to_local(utc_datetime):
     now_timestamp = time.time()
-    offset = datetime.datetime.fromtimestamp(now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
+    offset = datetime.datetime.fromtimestamp(
+        now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
     return utc_datetime + offset
+
 
 def shiftLocation(location):
     if(random.randrange(2) == 1):
@@ -31,12 +35,15 @@ def shiftLocation(location):
     else:
         return d(location) - d(random.randrange(10000000)/100000000)
 
+
 def getBikeLink():
     return f"https://www.google.com/maps/dir//{shiftLocation(lat)},{shiftLocation(lon)}"
-    
+
+
 def convertTemperature(temp):
     return round((temp - 273.15) * 9/5 + 32)
-    
+
+
 # context variables
 plantyStatus = secureData.variable("plantyStatus")
 now = datetime.datetime.now()
@@ -46,16 +53,16 @@ if(int(secureData.variable("walkAlertSent")) < (time.time() - 43200) and now.hou
     # Call API
     url_request = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={secureData.variable('weatherAPIKey')}"
     response = requests.get(url_request).json()
-    
+
     temperature = convertTemperature(response["current"]["temp"])
     low = convertTemperature(response["daily"][1]["temp"]["min"])
     high = convertTemperature(response["daily"][0]["temp"]["max"])
     wind = response["current"]["wind_speed"]
     sunset = response["daily"][0]["sunset"]
-    
+
     timeToSunset = (sunset - time.time()) / 3600
 
-    if(((temperature >= 65 and temperature <= 85) or (high >= 65 and high <=90)) and wind < 10 and timeToSunset > 2):
+    if(((temperature >= 65 and temperature <= 85) or (high >= 65 and high <= 90)) and wind < 10 and timeToSunset > 2):
         message = f"""\
             Hi Tyler,\
                 <br><br>Get walking, biking, and moving!<br><br>
@@ -65,19 +72,21 @@ if(int(secureData.variable("walkAlertSent")) < (time.time() - 43200) and now.hou
                     <li>The wind isn't bad</li>
                     <li>It's a nice time of day</li>
                     <br><br><h2><a href='{getBikeLink()}'>Here's a random place for you to go.</a></h2>"""
-        
+
         mail.send("Get outside today, please!", message)
-        secureData.write("walkAlertSent",str(int(time.time())))
+        secureData.write("walkAlertSent", str(int(time.time())))
         secureData.log("Walk Alert Sent")
-            
+
     # Planty Alerts
     if(int(secureData.variable("plantyAlertSent")) < (time.time() - 43200)):
         secureData.log("Checked Planty")
         if(low < 55 and plantyStatus == "out"):
-            mail.send("Take Planty In", "Hi Tyler,<br><br>The low tonight is {}°. Please take Planty in!".format(low))
+            mail.send(
+                f"""Take Planty In", "Hi Tyler,<br><br>The low tonight is {low}°. Please take Planty in!""")
             secureData.write("plantyStatus", "in")
             secureData.write("plantyAlertSent", str(int(time.time())))
         if(high > 80 and plantyStatus == "in"):
-            mail.send("Take Planty Out", "Hi Tyler,<br><br>It looks like a nice day! It's going to be around {}°. Please take Planty out.".format(high))
+            mail.send(
+                f"""Take Planty Out", "Hi Tyler,<br><br>It looks like a nice day! It's going to be around {high}°. Please take Planty out.""")
             secureData.write("plantyStatus", "out")
             secureData.write("plantyAlertSent", str(int(time.time())))
