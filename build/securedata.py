ee8506283a8b6f6296782e554fe9488b8c4f8bed
Arvind Deshpande <pilyad@gmail.com>
2022-01-08 10:05:15 -0600

    Added a default config file without being dependent on pi user home dir.

--- build/securedata.py
+++ build/securedata.py
@@ -1,15 +1,20 @@
 from datetime import datetime
 import os
 import sys
+import os.path
 
 # README
 # Builds my SecureData package; see https://pypi.org/project/securedata
 # see https://betterscientificsoftware.github.io/python-for-hpc/tutorials/python-pypi-packaging/ for specific instructions
 
+DEFAULT_CONFIG_FILE="~/securedata/setup.cfg"
+
 def main():
     # bump version
     try:
-        with open("/home/pi/Git/securedata/setup.cfg", 'r') as f:
+      if os.path.isfile( DEFAULT_CONFIG_FILE ):
+        # if default config file exists then read one.
+        with open(DEFAULT_CONFIG_FILE, 'r') as f:
             _f = f.read()
             originalVersionNumber = _f.split("version = ")[1].split("\n")[0]
             originalDate = '.'.join(originalVersionNumber.split(".")[:-1])
@@ -18,9 +23,12 @@ def main():
             newBuildNumber = 1 if originalDate != newDate else int(originalVersionNumber.split(".")[-1]) + 1
             newVersionNumber = f"""{datetime.now().strftime("%Y.%m.%d")}.{newBuildNumber}"""
             _f = _f.replace(originalVersionNumber,newVersionNumber)
-        with open("/home/pi/Git/securedata/setup.cfg", 'w') as f:
+        with open(DEFAULT_CONFIG_FILE, 'w') as f:
             f.write(_f)
             print(f"Bumped version to {newVersionNumber}")
+      else:
+        #sys.exit("[%s] does not exist!" % DEFAULT_CONFIG_FILE)
+        print("[%s] does not exist!" % DEFAULT_CONFIG_FILE)
     except Exception as e:
         print(e)
         sys.exit("Could not parse setup.cfg to determine incremented version number")
@@ -40,3 +48,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
