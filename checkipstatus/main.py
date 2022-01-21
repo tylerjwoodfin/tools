6403525022dbaaa40e65dcd2e990256f206bf016
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-01-20 21:43:41 -0600

    Updated checkipstatus to new securedata

--- checkipstatus/main.py
+++ checkipstatus/main.py
@@ -1,43 +1,36 @@
 import json
-import os
-import sys
 from requests import get
+from securedata import securedata, mail
 
-# for `mail`
-currentdir = os.path.dirname(os.path.realpath(__file__))
-parentdir = os.path.dirname(currentdir)
-sys.path.append(parentdir)
+currentIP = securedata.getItem("currentIP")
 
-from securedata import securedata
-import mail
-
-currentIP = securedata.variable("currentIP")
-email = securedata.variable("email")
-
-print("Updating IP Address, " + email)
+print("Updating IP Address")
 
 ip = get('https://api.ipify.org?format=json').text
 discoveredIP = json.loads(ip)["ip"]
 
-print ("Found " + discoveredIP + ", currently " + currentIP + ".")
+print(f"Found {discoveredIP}, currently {currentIP}")
 
 # IP was updated
 if currentIP == discoveredIP:
     print("No change.")
 else:
     print("New IP! Updating and sending email.")
-    securedata.write("currentIP", discoveredIP)
+    securedata.setItem("currentIP", discoveredIP)
     
     # Replace OVPN file with new IP
-    newLine = "remote " + discoveredIP + " 1194"
-    vpnFile = securedata.file("tyler.cloud.ovpn", "/home/pi/ovpns")
-    
-    vpnFile = vpnFile.split("remote ")[0] + newLine + vpnFile.split(" 1194")[1]
-    securedata.write("tyler.cloud.ovpn", vpnFile, "/home/pi/ovpns")
+    newLine = f"remote {discoveredIP} 1194"
+
+    try:
+        vpnFile = '\n'.join(securedata.getFileAsArray("tyler.cloud.ovpn", "/home/pi/ovpns/"))
+        vpnFile = vpnFile.split("remote ")[0] + newLine + vpnFile.split(" 1194")[1]
+        securedata.writeFile("tyler.cloud.ovpn", "/home/pi/ovpns/", vpnFile )
+    except Exception as e:
+        securedata.log(f"Could not update VPN: {e}", level="error")
     
     # Send email
-    message = """
-    Your public IP was updated from %s to %s. To keep tyler.cloud, update your Namecheap settings.<br><br>
-    To keep TylerVPN, please switch your OVPN file to the one located in (probably) Dropbox/Backups/securedata.""" % (currentIP, discoveredIP)
+    message = f"""
+    Your public IP was updated from {currentIP} to {discoveredIP}. To keep tyler.cloud, update your Namecheap settings.<br><br>
+    To keep TylerVPN, please switch your OVPN file to the one located in (probably) Dropbox/securedata."""
     
     mail.send("IP Updated - new OVPN file", message)
