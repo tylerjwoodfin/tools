2d70dce82aef826d141124252571a0d1787fe345
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-04-11 23:43:48 -0500

    Changed behavior to ssh into the web server

--- shorten.py
+++ shorten.py
@@ -1,14 +1,14 @@
 import random
 import string
-import securedata
+from securedata import securedata
 from os import system
 from sys import argv, exit
 
 """
 Used by tyler.cloud to provide a faster URL shortener
 
-If this is stored in a web server, one could add this to their .bashrc for rapid URL shortening:
-alias shorten="ssh -oHostKeyAlgorithms=+ssh-dss -p {portNumber} {username}@{server} 'python3 {path}/tools/shorten.py"
+If this is stored in a web server, one could store this in securedata -> shorten_ssh:
+"ssh -oHostKeyAlgorithms=+ssh-dss -p {portNumber} {username}@{server} "
 """
 
 
@@ -20,9 +20,12 @@ def getUrl():
 if len(argv) < 2:
     print("Error- missing url; usage: `shorten url`")
     exit(-1)
-with open('~/www/.htaccess', 'a') as f:
-    directory = getUrl()
-    f.write(
-        f"\n\nRewriteCond %{{REQUEST_URI}} ^/u/{directory}.*\nRewriteRule (.*) {argv[1]}")
-    print(f"https://tyler.cloud/u/{directory}")
-    exit(1)
+
+if not argv[1].startswith('http'):
+    print("Error- make sure to provide the complete URL.")
+    exit(-1)
+
+directory = getUrl()
+system(
+    f"""{securedata.getItem('shorten_ssh')} "echo '\nRewriteCond %{{REQUEST_URI}} ^/u/{directory}.*\nRewriteRule (.*) {argv[1]}' >> www/.htaccess" """)
+print(f"https://tyler.cloud/u/{directory}")
