15c55e3a402ea689650fd46c7c53571c8a39bfb3
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-06-21 21:55:15 -0500

    Moved kasalights to this repo; added support for sys.argv device selection

--- kasalights/main.py
+++ kasalights/main.py
@@ -1,10 +1,9 @@
 from os import system as run
 import kasa
-import json
+import re
+import sys
 import asyncio
 from securedata import securedata
-
-# run("kasa --host 192.168.0.212 --type dimmer off")
 
 dimmers = kasa.Discover()
 
@@ -13,6 +12,7 @@ devices = securedata.getItem("lights") or {}
 
 async def main():
 
+    # discover devices
     if devices == {}:
         print("Discovering devices...")
         found_devices = await kasa.Discover.discover()
@@ -22,7 +22,18 @@ async def main():
 
         securedata.setItem("lights", devices)
 
+    # parse args
     device = ''
+    operation = ''
+    if len(sys.argv) > 2:
+        if 'on' in sys.argv[1:]:
+            operation = 'on'
+        elif 'off' in sys.argv[1:]:
+            operation = 'off'
+
+        device = ' '.join(
+            list(filter(lambda x: x != 'on' and x != 'off', sys.argv[1:])))
+
     devices_keys = devices.keys()
     while device not in devices_keys and f"{device} light" not in devices_keys:
         device = input("\nWhat would you like to toggle?\n")
@@ -33,14 +44,20 @@ async def main():
     if not device.endswith("light") and device not in devices_keys:
         device = f"{device} light"
 
+    # get dimmer state
     dimmer = kasa.SmartDimmer(devices[device])
-
     await dimmer.update()
+
+    # handle already on/off
+    if dimmer.is_on and operation == 'on' or dimmer.is_off and operation == 'off':
+        print(f"The {device} is already {operation}")
+        sys.exit(0)
+
     print(f"Turning {device} {'off' if dimmer.is_on else 'on'}")
 
-    if dimmer.is_on:
+    if dimmer.is_on and operation != 'off':
         await dimmer.turn_off()
-    else:
+    elif operation != 'off':
         await dimmer.turn_on()
 
 if __name__ == "__main__":
