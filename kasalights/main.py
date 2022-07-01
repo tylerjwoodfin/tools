e2fe41df1ab7ca7c82809f782f2faa13824c7525
Tyler Woodfin <tyler@tylerwoodfin.com>
2022-06-30 22:32:47 -0500

    Added 'all' support to kasa lights

--- kasalights/main.py
+++ kasalights/main.py
@@ -1,6 +1,5 @@
 from os import system as run
 import kasa
-import re
 import sys
 import asyncio
 from securedata import securedata
@@ -8,6 +7,7 @@ from securedata import securedata
 dimmers = kasa.Discover()
 
 devices = securedata.getItem("lights") or {}
+devices_keys = devices.keys()
 
 
 async def main():
@@ -25,21 +25,34 @@ async def main():
     # parse args
     device = ''
     operation = ''
-    if len(sys.argv) > 2:
+    if len(sys.argv) >= 2:
         if 'on' in sys.argv[1:]:
             operation = 'on'
         elif 'off' in sys.argv[1:]:
             operation = 'off'
 
+        # handle "all"
+        if 'all' in sys.argv[1:]:
+            for device in devices:
+                await switch(device, operation)
+            sys.exit(0)
+
         device = ' '.join(
             list(filter(lambda x: x != 'on' and x != 'off', sys.argv[1:])))
 
-    devices_keys = devices.keys()
     while device not in devices_keys and f"{device} light" not in devices_keys:
         device = input("\nWhat would you like to toggle?\n")
 
         if device not in devices_keys and f"{device} light" not in devices_keys:
             print(f"\n\n{device} not in {list(devices_keys)}")
+
+    switch(device, operation or "toggle")
+
+
+async def switch(device, operation):
+    if device not in devices_keys and f"{device} light" not in devices_keys:
+        print(f"\n\n{device} not in {list(devices_keys)}")
+        return
 
     if not device.endswith("light") and device not in devices_keys:
         device = f"{device} light"
@@ -48,10 +61,17 @@ async def main():
     dimmer = kasa.SmartDimmer(devices[device])
     await dimmer.update()
 
+    # handle toggle
+    if operation == 'toggle':
+        if dimmer.is_on:
+            operation == 'off'
+        else:
+            operation == 'on'
+
     # handle already on/off
     if dimmer.is_on and operation == 'on' or dimmer.is_off and operation == 'off':
         print(f"The {device} is already {operation}")
-        sys.exit(0)
+        return
 
     print(f"Turning {device} {'off' if dimmer.is_on else 'on'}")
 
