7a532a55e8f34b4e41691cbf2a973f4283f7c672
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-09-09 23:16:35 -0500

    Set workout to use Dropbox file; added logs

--- workout/main.py
+++ workout/main.py
@@ -1,25 +1,20 @@
-import os
 import time
-from securedata import securedata, mail
 import datetime
+from securedata import securedata, mail
 from sys import exit
 
 TODAY = datetime.date.today()
 DAY_EPOCH = int(int(time.time())/60/60/24)
-WORKOUT_FILE_PATH = os.path.dirname(os.path.realpath(__file__))
 WORKOUT_FILE = '<br>'.join(securedata.getFileAsArray(
-    "README.md", WORKOUT_FILE_PATH)).split("### ")
-WORKOUT_FILE_LEN = len(WORKOUT_FILE) - 1
+    "workout.md", "notes")).split("### ")
+
+securedata.log("Checking workout")
 
 if TODAY.weekday() == 5:
     # ignore no-obligation Saturdays
+    securedata.log("Saturday - no workout to be sent")
     exit(0)
 
-message = f"Hi Tyler,\n\nhere's your workout for {WORKOUT_FILE[(DAY_EPOCH % WORKOUT_FILE_LEN) + 1]}"
-
-if (DAY_EPOCH % WORKOUT_FILE_LEN) % 2 == 0:
-    message += f"\nIf you have a training session today, this is <b>in addition to</b> that."
-else:
-    message += f"\nIf you have a training session today, you can ignore this."
+message = f"Hi Tyler,\n\nHere's your workout for {WORKOUT_FILE[(TODAY.weekday()) + 1]}"
 
 mail.send(f"Your Workout, {TODAY}", message)
