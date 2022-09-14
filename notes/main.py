d42434d0dc12fcaa452a53bf91e3ce0061cd8a7e
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-09-13 23:18:25 -0500

    Added error handling and extension handling

--- notes/main.py
+++ notes/main.py
@@ -4,19 +4,25 @@ Notes
 Edits a file in the notes folder based on argv[1]; can create file if it doesn't exist
 """
 
-from sys import argv
+import sys
 from os import listdir
 from securedata import securedata
+
+if len(sys.argv) < 2:
+    print("Usage: `notes {filename in securedata notes folder}`")
+    sys.exit(-1)
+elif sys.argv[1].endswith('.md'):
+    sys.argv[1] = sys.argv[1].split(".md", maxsplit=1)[0]
 
 # print(securedata.editFile(argv[1],  ))
 PATH_FILE_TO_EDIT = securedata.getItem("path", "notes", "local")
 
-if securedata.editFile(f"{PATH_FILE_TO_EDIT}/{argv[1]}.md", sync=True) == -1:
+if securedata.editFile(f"{PATH_FILE_TO_EDIT}/{sys.argv[1]}.md", sync=True) == -1:
 
     print('\nAVAILABLE FILES:')
     print('\n'.join(listdir(PATH_FILE_TO_EDIT)))
     print('\n')
 
-    if input(f"Do you want to create {argv[1]}.md?\n\n").startswith("y"):
+    if input(f"Do you want to create {sys.argv[1]}.md?\n\n").startswith("y"):
         securedata.editFile(
-            f"{PATH_FILE_TO_EDIT}/{argv[1]}.md", sync=True, createIfNotExist=True)
+            f"{PATH_FILE_TO_EDIT}/{sys.argv[1]}.md", sync=True, createIfNotExist=True)
