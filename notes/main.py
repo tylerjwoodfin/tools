2a8e4b5c5c00890aed6657502c2255cb7586fa61
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-09-13 23:05:41 -0500

    Added notes script for easy note editing

--- notes/main.py
+++ notes/main.py
@@ -1,5 +1,22 @@
-from securedata import securedata
+"""
+Notes
+
+Edits a file in the notes folder based on argv[1]; can create file if it doesn't exist
+"""
+
 from sys import argv
+from os import listdir
+from securedata import securedata
 
 # print(securedata.editFile(argv[1],  ))
-print(securedata.getItem("path", "notes", "local"))
+PATH_FILE_TO_EDIT = securedata.getItem("path", "notes", "local")
+
+if securedata.editFile(f"{PATH_FILE_TO_EDIT}/{argv[1]}.md", sync=True) == -1:
+
+    print('\nAVAILABLE FILES:')
+    print('\n'.join(listdir(PATH_FILE_TO_EDIT)))
+    print('\n')
+
+    if input(f"Do you want to create {argv[1]}.md?\n\n").startswith("y"):
+        securedata.editFile(
+            f"{PATH_FILE_TO_EDIT}/{argv[1]}.md", sync=True, createIfNotExist=True)
