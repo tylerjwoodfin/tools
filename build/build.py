0cf5feae1b522fd36dcbdd912caae1363b96123f
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-09-25 23:15:16 -0500

    Removed Evolve; improved build.py to conform to Pylint standards

--- build/build.py
+++ build/build.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-from securedata import securedata
-import sys
-import os.path
-
-
 """
 This tool is specific to the original developer and is used to publish to PyPi.
 """
+
+import sys
+import os.path
+from datetime import datetime
+from securedata import securedata
 
 BUILD_OPTIONS = ['securedata', 'remindmail']
 
@@ -18,8 +17,10 @@ if len(sys.argv) < 2 or sys.argv[1] not in BUILD_OPTIONS:
 
 PATH_SRC_SECUREDATA = securedata.getItem("path", "securedata", "src")
 PATH_SRC_REMINDMAIL = securedata.getItem("path", "remindmail", "src")
-DEFAULT_CONFIG_FILE_SECUREDATA = f'{PATH_SRC_SECUREDATA}/setup.cfg' or f'{os.path.expanduser("~")}/securedata/setup.cfg'
-DEFAULT_CONFIG_FILE_REMINDMAIL = f'{PATH_SRC_REMINDMAIL}/setup.cfg' or f'{os.path.expanduser("~")}/remindmail/setup.cfg'
+DEFAULT_CONFIG_FILE_SECUREDATA = f'{PATH_SRC_SECUREDATA}/setup.cfg' or \
+    f'{os.path.expanduser("~")}/securedata/setup.cfg'
+DEFAULT_CONFIG_FILE_REMINDMAIL = f'{PATH_SRC_REMINDMAIL}/setup.cfg' or \
+    f'{os.path.expanduser("~")}/remindmail/setup.cfg'
 
 PATH_SRC = PATH_SRC_REMINDMAIL if sys.argv[1] == 'remindmail' else PATH_SRC_SECUREDATA
 DEFAULT_CONFIG_FILE = DEFAULT_CONFIG_FILE_REMINDMAIL if sys.argv[
@@ -27,34 +28,40 @@ DEFAULT_CONFIG_FILE = DEFAULT_CONFIG_FILE_REMINDMAIL if sys.argv[
 
 
 def main():
-    # bump version
+    """
+    Bump version of config file, then build and upload using twine
+    """
+
+    # bump version to YYYY.MM.DD.n
     try:
         if os.path.isfile(DEFAULT_CONFIG_FILE):
-            with open(DEFAULT_CONFIG_FILE, 'r') as f:
-                _f = f.read()
-                originalVersionNumber = _f.split(
+            with open(DEFAULT_CONFIG_FILE, 'r', encoding="utf8") as file_config:
+                _file_config = file_config.read()
+                original_version_number = _file_config.split(
                     "version = ")[1].split("\n")[0]
-                originalDate = '.'.join(originalVersionNumber.split(".")[:-1])
-                newDate = datetime.now().strftime("%Y.%m.%d")
+                original_date = '.'.join(
+                    original_version_number.split(".")[:-1])
+                new_date = datetime.now().strftime("%Y.%m.%d")
 
-                newBuildNumber = 1 if originalDate != newDate else int(
-                    originalVersionNumber.split(".")[-1]) + 1
-                newVersionNumber = f"""{datetime.now().strftime("%Y.%m.%d")}.{newBuildNumber}"""
-                _f = _f.replace(originalVersionNumber, newVersionNumber)
-            with open(DEFAULT_CONFIG_FILE, 'w') as f:
-                f.write(_f)
-                print(f"Bumped version to {newVersionNumber}")
+                new_build_number = 1 if original_date != new_date else int(
+                    original_version_number.split(".")[-1]) + 1
+                new_version_number = f"""{datetime.now().strftime("%Y.%m.%d")}.{new_build_number}"""
+                _file_config = _file_config.replace(
+                    original_version_number, new_version_number)
+            with open(DEFAULT_CONFIG_FILE, 'w', encoding="utf8") as file_config:
+                file_config.write(_file_config)
+                print(f"Bumped version to {new_version_number}")
         else:
             sys.exit(f"Cannot build; {DEFAULT_CONFIG_FILE} does not exist")
-    except Exception as e:
-        print(e)
+    except IOError as error:
+        print(error)
         sys.exit("Could not parse setup.cfg to determine incremented version number")
 
     # delete `dist` directory
     try:
         os.system(f"rm -rf {PATH_SRC}/dist")
-    except Exception as e:
-        print(f"Warning: {e}")
+    except IOError as error:
+        print(f"Warning: {error}")
 
     # build
     print("Building... this will take a few minutes")
