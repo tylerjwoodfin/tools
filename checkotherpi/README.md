1a76e12c96007d0c7743cc005e88b6d43c2b5b61
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-09-25 23:24:04 -0500

    Modified checkotherpi to conform to Pylint standards

--- checkotherpi/README.md
+++ checkotherpi/README.md
@@ -1,10 +1,14 @@
 # checkwebsite
-- My website is hosted on my Raspberry Pi 4 at 192.168.1.123. I have a second Raspberry Pi at 192.168.1.124. 
+
+- I have 2 Raspberry Pis (192.168.0.123, 192.168.0.124)
 - This tool pings the sister device, and if after 3 attempts, it cannot be reached, it sends an email.
+
 # Dependencies
+
 - `../mail.py` should be configured using [securedata](https://pypi.org/project/securedata)
-    - Otherwise, you can comment out `mail` lines and use `print`.
+  - Otherwise, you can comment out `mail` lines and use `print`.
 
 # Usage
+
 - `python3 /path/to/main.py`
 - I recommend putting this in your crontab (`crontab -e`)
--- checkotherpi/main.py
+++ checkotherpi/main.py
@@ -1,3 +1,6 @@
+"""
+checkotherpi - see README.md
+"""
 import os
 import sys
 import socket
@@ -6,21 +9,22 @@ from securedata import mail
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-myIP = s.getsockname()[0]
+MY_IP = s.getsockname()[0]
 s.close()
 
-hostname = "192.168.0.123"
+HOSTNAME = "192.168.0.123"
 
-if myIP == "192.168.0.123":
-    hostname = "192.168.0.124"
+if MY_IP == "192.168.0.123":
+    HOSTNAME = "192.168.0.124"
 
-response = -1
+RESPONSE = -1
 
 for i in range(2):
-    response = os.system("ping -c 1 " + hostname)
+    RESPONSE = os.system("ping -c 1 " + HOSTNAME)
 
-    if response == 0:
+    if RESPONSE == 0:
         sys.exit(0)
 
-print(response)
-mail.send(f"{hostname} is Down", f"Your server at " + hostname + " is down.")
+# sister device not found
+print(RESPONSE)
+mail.send(f"{HOSTNAME} is Down", f"Your server at {HOSTNAME} is down.")
