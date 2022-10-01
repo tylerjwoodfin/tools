91005f17bdee617714990f2e95bef4201e94fe5f
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-10-01 00:55:30 -0500

    Added workout types to emails

--- workout/main.py
+++ workout/main.py
@@ -1,5 +1,16 @@
 """
 Sends a workout description based on a workout markdown file; scheduled in crontab
+
+Format:
+
+## DAYW
+### Type
+
+- instruction 1
+- instruction 2
+
+## DAYW + 1
+...
 """
 
 import sys
@@ -10,7 +21,10 @@ from securedata import securedata, mail
 TODAY = datetime.date.today()
 DAY_EPOCH = int(int(time.time())/60/60/24)
 WORKOUT_FILE = '<br>'.join(securedata.getFileAsArray(
-    "workout.md", "notes")).split("### ")
+    "workout.md", "notes")).split("## ")[(TODAY.weekday()) + 1].split("<br>")
+
+WORKOUT_MSG = '<br>'.join(WORKOUT_FILE[1:])
+WORKOUT_TYPE = WORKOUT_FILE[0]
 
 securedata.log("Checking workout")
 
@@ -19,6 +33,6 @@ if TODAY.weekday() == 5:
     securedata.log("Saturday - no workout to be sent")
     sys.exit(0)
 
-message = f"Hi Tyler,\n\nHere's your workout for {WORKOUT_FILE[(TODAY.weekday()) + 1]}"
+message = f"Hi Tyler,\n\nHere's your {WORKOUT_TYPE} workout for today: {WORKOUT_MSG}"
 
-mail.send(f"Your Workout, {TODAY}", message)
+mail.send(f"{WORKOUT_TYPE} for {TODAY}", message)
