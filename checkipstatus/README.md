d00f011196519e2c25fa35273efbbb8be9c1a7bd
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2022-10-25 23:36:53 -0500

    Updated IP address checker

--- checkipstatus/README.md
+++ checkipstatus/README.md
@@ -1,7 +1,6 @@
-# Dependencies
-pip install miniupnpc
+# checkipstatus
+My PiVPN needs a constant IP, and since I don't have a static IP and don't want to use ddns.net, I have this script which checks my IP address and updates the OVPN file, then emails me.
 
-Email: see `mail.py`. I'm currently using msmtp.
-
-# Description
-My PiVPN needs a constant IP, and since I don't have a static IP and don't want to use ddns.net,  I have this script which checks my IP address and updates the OVPN file, then emails me.
+# dependencies
+- [securedata](https://github.com/tylerjwoodfin/securedata)
+    - set up mail per the securedata README
--- checkipstatus/main.py
+++ checkipstatus/main.py
@@ -6,39 +6,24 @@ import json
 from requests import get
 from securedata import securedata, mail
 
-currentIP = securedata.getItem("currentIP")
+ip_current = securedata.getItem("currentIP")
 
-print("Updating IP Address")
+securedata.log("Checking IP")
 
 ip = get('https://api.ipify.org?format=json', timeout=30).text
-discovered_IP = json.loads(ip)["ip"]
+ip_discovered = json.loads(ip)["ip"]
 
-print(f"Found {discovered_IP}, currently {currentIP}")
+securedata.log(f"Found {ip_discovered}, currently {ip_current}")
 
-# IP was updated
-if currentIP == discovered_IP:
+if ip_current == ip_discovered:
     print("No change.")
 else:
-    print("New IP! Updating and sending email.")
-    securedata.setItem("currentIP", discovered_IP)
-
-    # Replace OVPN file with new IP
-    newLine = f"remote {discovered_IP} 1194"
-
-    try:
-        FILE_VPN = '\n'.join(securedata.getFileAsArray(
-            "tyler.cloud.ovpn", "/home/tyler/ovpns/"))
-        FILE_VPN = FILE_VPN.split("remote ", maxsplit=1)[
-            0] + newLine + FILE_VPN.split(" 1194")[1]
-        securedata.writeFile("tyler.cloud.ovpn",
-                             "/home/tyler/ovpns/", FILE_VPN)
-    except IOError as e:
-        securedata.log(f"Could not update VPN: {e}", level="error")
-
-    # Send email
-    message = (f"Your public IP was updated from {currentIP} to {discovered_IP}."
-               "To keep tyler.cloud, update your Namecheap settings.<br><br>"
-               "To keep TylerVPN, please switch your OVPN file to the one located in"
-               " (probably) Dropbox/securedata.")
-
-    mail.send("IP Updated - new OVPN file", message)
+    msg = f"""Hi Tyler,<br><br>
+    Your IP address was changed from {ip_current} to {ip_discovered}.<br><br>
+    Please update /etc/wireguard/wg0.conf accordingly.
+    """
+    
+    mail.send("IP Address Updated", msg)
+    
+    # update securedata
+    securedata.setItem("currentIP", ip_discovered)
