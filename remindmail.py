ad6b6e3c2a351e7229c32347ad828a1af5d1354b
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2023-01-30 23:20:47 -0600

    Added mail option in remindmail cli; fixed weather alerts for updated securedata mail

--- remindmail.py
+++ remindmail.py
@@ -3,12 +3,19 @@ in order to use remindmail directly, this wrapper function is used.
 """
 import sys
 from remind import remind
-from securedata import securedata
+from securedata import securedata, mail
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'generate':
         securedata.log("Calling remind generate")
         remind.generate()
-    elif sys.argv[1] == 'later':
+    if sys.argv[1] == 'later':
         securedata.log("Calling remind later")
         remind.generate_reminders_for_later()
+    if sys.argv[1] == 'mail':
+        if len(sys.argv) < 5:
+            print("Usage: remind mail <subject> <body> <to_addr, comma-separated>")
+        else:
+            mail.send(subject=sys.argv[2], body=sys.argv[3], to_addr=sys.argv[4].split(","))
+    else:
+        print(f"Invalid command: {sys.argv[1]}")
--- weather_alerts.py
+++ weather_alerts.py
@@ -125,8 +125,8 @@ if len(sys.argv) > 1 and sys.argv[1] == 'force' or \
         try:
             mail.send("Take Planty In This Afternoon",
                       (f"Hi Tyler,<br><br>The low tonight is {low_tomorrow}°."
-                       f" Please take Planty in!"), to_addr=','.join(
-                          securedata.getItem("weather", "alert_planty_emails")))
+                       f" Please take Planty in!"),
+                      to_addr=securedata.getItem("weather", "alert_planty_emails"))
             securedata.setItem(
                 "weather", "alert_planty_sent", int(time.time()))
         except IOError as e:
@@ -137,8 +137,8 @@ if len(sys.argv) > 1 and sys.argv[1] == 'force' or \
             EMAIL = (f"Hi Tyler,<br><br>It looks like a nice day!"
                      f" It's going to be around {high}°. Please take Planty out.")
 
-            mail.send("Take Planty Out This Afternoon", EMAIL, to_addr=','.join(
-                securedata.getItem("weather", "alert_planty_emails")))
+            mail.send("Take Planty Out This Afternoon", EMAIL,
+                      to_addr=securedata.getItem("weather", "alert_planty_emails"))
             securedata.setItem(
                 "weather", "alert_planty_sent", int(time.time()))
         except IOError as e:
