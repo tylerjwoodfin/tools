5d90ca1d4f3a4bff6000762e900bb76f6adda922
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2023-04-17 22:20:18 -0500

    Adjusted bedtime.py

--- ubuntu/bedtime.py
+++ ubuntu/bedtime.py
@@ -11,7 +11,7 @@ Dependencies for Python:
 tkinter (sudo apt-get install python3-tk)
 
 Example Crontab:
-0 22 * * * cd /home/tyler/git/tools/ubuntu && XAUTHORITY=/home/tyler/.Xauthority DISPLAY=:0 python3 bedtime.py
+0 1 * * * cd <bedtime.py folder> && XAUTHORITY=/home/<you>/.Xauthority DISPLAY=:0 python3 bedtime.py
 """
 
 #!/usr/bin/env python
@@ -25,34 +25,38 @@ from tkinter.ttk import Label
 def _disable_event():
     pass
 
+
 class App:
     """
     contains the description of the fullscreen, blocking window and text that is displayed
     """
 
     def __init__(self, master, message) -> None:
-
         # Instantiating master i.e toplevel Widget
         self.master = master
         self.text = tk.StringVar()
 
         # block alt-tab
-        print(os.system("GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.wm.keybindings switch-applications \"[]\""))
+        print(os.system(
+            "GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.wm.keybindings "
+            "switch-applications \"[]\""
+        ))
 
         # Creating first Label i.e with default font-size
         self.title = Label(
             self.master, textvariable=self.text, font=("Arial", 25))
         self.title.pack(pady=20, expand=True)
 
-        self.timeremaining = 200
+        self.timeremaining = 10
 
         while self.timeremaining > 0:
             self.text.set((f"{message}\nThis message will remain for "
-                           f"{self.timeremaining}"
-                           f" more {'second' if self.timeremaining == 1 else 'seconds'}."))
+                           f"{self.timeremaining} more "
+                           f"{'second' if self.timeremaining == 1 else 'seconds'}."))
             root.update()
             self.timeremaining -= 1
             time.sleep(1)
+
 
 def restore():
     """
@@ -60,23 +64,23 @@ def restore():
     """
     print(os.system(CMD_ALT_TAB))
 
+
 if __name__ == "__main__":
-    CMD_ALT_TAB = "GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.wm.keybindings" \
-        " switch-applications \"['<Alt>Tab']\""
+    CMD_ALT_TAB = "GSETTINGS_BACKEND=dconf /usr/bin/gsettings set "
+    "org.gnome.desktop.wm.keybindings switch-applications \"['<Alt>Tab']\""
 
     root = tk.Tk()
     root.attributes('-fullscreen', True)
     root.protocol("WM_DELETE_WINDOW", _disable_event)
     root.title("Get Ready for Bed")
-    root.after(200000, restore)
-    root.after(200000, root.destroy)
+    root.after(10000, restore)
+    root.after(10000, root.destroy)
 
     # initialize
     if len(sys.argv) > 1:
         MESSAGE = sys.argv[1]
     else:
-        MESSAGE = "Please get ready for bed.\nYour bedtime is 11:30 tonight."
-        "\nMelatonin is a good idea."
+        MESSAGE = "Medicine and Melatonin!"
 
     app = App(root, MESSAGE)
     root.mainloop()
