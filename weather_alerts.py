5f93e6214483bf62235d3228fb2543a0c9f154a8
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2023-10-07 23:19:55 -0700

    ANT-101: Implemented air quality checking

--- weather_alerts.py
+++ weather_alerts.py
@@ -28,7 +28,8 @@ def datetime_from_utc_to_local(utc_datetime):
     """
     now_timestamp = time.time()
     offset = datetime.datetime.fromtimestamp(
-        now_timestamp) - datetime.datetime.utcfromtimestamp(now_timestamp)
+        now_timestamp
+    ) - datetime.datetime.utcfromtimestamp(now_timestamp)
     return utc_datetime + offset
 
 
@@ -37,52 +38,95 @@ def shift_location(location):
     generates a random offset from the current location to find a place to walk/bike
     """
     if random.randrange(2) == 1:
-        return float(location) * (.9999) + (random.randrange(1, 100) / 10000)
+        return float(location) * (0.9999) + (random.randrange(1, 100) / 10000)
 
-    return float(location) * (.9999) - (random.randrange(1, 100) / 10000)
+    return float(location) * (0.9999) - (random.randrange(1, 100) / 10000)
 
 
 def get_location_link():
     """
     returns a Google Maps URL for the shifted location
     """
-    return f"https://www.google.com/maps/dir/{shift_location(lat)},{shift_location(lon)}"
+    return (
+        f"https://www.google.com/maps/dir/{shift_location(lat)},{shift_location(lon)}"
+    )
 
 
 def convert_temperature_c_to_f(temp):
     """
     converts from celsius to fahrenheit
     """
-    return round((temp - 273.15) * 9/5 + 32)
+    return round((temp - 273.15) * 9 / 5 + 32)
+
+def get_air_quality_advice(aqi_components):
+    """
+    Gets advice based on air quality
+    """
+
+    # Setting some basic thresholds (µg/m³)
+    pm25_threshold = 12
+    pm10_threshold = 20
+    o3_threshold = 70
+    no2_threshold = 25
+
+    components = aqi_components
+
+    advice = []
+    if components["pm2_5"] > pm25_threshold:
+        advice.append("High PM2.5 levels. Consider an N95 mask outdoors.")
+    if components["pm10"] > pm10_threshold:
+        advice.append("Elevated PM10. Consider short walks with an N95 mask.")
+    if components["o3"] > o3_threshold:
+        advice.append("High Ozone levels. Limit outdoor time & strenuous activities.")
+    if components["no2"] > no2_threshold:
+        advice.append("High NO2 levels. Maybe stay indoors or limit exposure.")
+    return advice
 
 
 # context variables
-plantyStatus = cab.get("planty", "status")
+planty_status = cab.get("planty", "status")
 now = datetime.datetime.now()
 
 # Call API
-url_request = (f"https://api.openweathermap.org/data/2.5/onecall"
-               f"?lat={lat}&lon={lon}&appid={cab.get('weather', 'api_key')}")
-print(f"Calling API at {url_request}")
 
-response = requests.get(url_request, timeout=30).json()
+# ... weather
+url_request_weather = (
+    f"https://api.openweathermap.org/data/2.5/onecall"
+    f"?lat={lat}&lon={lon}&appid={cab.get('weather', 'api_key')}"
+)
+print(f"Calling API at {url_request_weather}")
 
-temperature = convert_temperature_c_to_f(response["current"]["temp"])
-conditions_now = response["current"]["weather"][0]["description"]
-conditions_now_icon = response["current"]["weather"][0]["icon"]
-conditions_tomorrow = response["daily"][1]["weather"][0]["description"]
-high_tomorrow = convert_temperature_c_to_f(response["daily"][1]["temp"]["max"])
-low_tomorrow = convert_temperature_c_to_f(response["daily"][1]["temp"]["min"])
+# ... air quality
+url_request_air = (
+    f"http://api.openweathermap.org/data/2.5/air_pollution"
+    f"?lat={lat}&lon={lon}&appid={cab.get('weather', 'api_key')}"
+)
+
+
+response_weather = requests.get(url_request_weather, timeout=30).json()
+
+print(f"Calling API at {url_request_air}")
+response_air = requests.get(url_request_air, timeout=30).json()
+response_air_components = response_air["list"][0]["components"]
+
+temperature = convert_temperature_c_to_f(response_weather["current"]["temp"])
+conditions_now = response_weather["current"]["weather"][0]["description"]
+conditions_now_icon = response_weather["current"]["weather"][0]["icon"]
+conditions_tomorrow = response_weather["daily"][1]["weather"][0]["description"]
+high_tomorrow = convert_temperature_c_to_f(response_weather["daily"][1]["temp"]["max"])
+low_tomorrow = convert_temperature_c_to_f(response_weather["daily"][1]["temp"]["min"])
 sunrise_tomorrow_formatted = time.strftime(
-    '%Y-%m-%d %H:%M AM', time.localtime(response["daily"][1]["sunrise"]))
+    "%Y-%m-%d %H:%M AM", time.localtime(response_weather["daily"][1]["sunrise"])
+)
 sunset_tomorrow_formatted = time.strftime(
-    '%Y-%m-%d %I:%M PM', time.localtime(response["daily"][1]["sunset"]))
-humidity = response["current"]["humidity"]
+    "%Y-%m-%d %I:%M PM", time.localtime(response_weather["daily"][1]["sunset"])
+)
+humidity = response_weather["current"]["humidity"]
 
-high = convert_temperature_c_to_f(response["daily"][0]["temp"]["max"])
-wind = response["current"]["wind_speed"]
-sunset = response["daily"][0]["sunset"]
-timeToSunset = (sunset - time.time()) / 3600
+high = convert_temperature_c_to_f(response_weather["daily"][0]["temp"]["max"])
+wind = response_weather["current"]["wind_speed"]
+sunset = response_weather["daily"][0]["sunset"]
+time_to_sunset = (sunset - time.time()) / 3600
 
 weather_data = {
     "current_temperature": temperature,
@@ -93,13 +137,28 @@ weather_data = {
     "tomorrow_low": low_tomorrow,
     "tomorrow_conditions": conditions_tomorrow,
     "tomorrow_sunrise": sunrise_tomorrow_formatted,
-    "tomorrow_sunset": sunset_tomorrow_formatted}
+    "tomorrow_sunset": sunset_tomorrow_formatted,
+}
 
 cab.put("weather", "data", weather_data)
 
-if cab.get("weather", "alert_walk_sent") < (time.time() - 43200) and 10 <= now.hour < 19:
+if (
+    cab.get("weather", "alert_walk_sent") < (time.time() - 43200)
+    and 10 <= now.hour < 19
+):
+    # send air quality alerts
+    air_quality_advice = get_air_quality_advice(response_air_components)
+    if len(air_quality_advice) > 0:
+        AIR_QUALITY_ADVICE = '\n'.join([f"<li>{item}</li>" for item in air_quality_advice])
+        message = f"""\
+            Hi Tyler,\
+            <br><br>Take a look at today's air quality:<br><br>
+            <ul>{AIR_QUALITY_ADVICE}</ul>"""
+        mail.send("Air Quality Alert", message)
+
+    # air quality is fine
     GOOD_TEMP = (65 <= temperature <= 85) or (72 <= temperature <= 90)
-    if GOOD_TEMP and wind < 10 and timeToSunset > 2:
+    if GOOD_TEMP and wind < 10 and time_to_sunset > 2:
         message = f"""\
             Hi Tyler,\
                 <br><br>It's very specifically nice outside!<br><br>
@@ -108,6 +167,7 @@ if cab.get("weather", "alert_walk_sent") < (time.time() - 43200) and 10 <= now.h
                     <li>It's not raining</li>
                     <li>The wind isn't bad</li>
                     <li>It's a nice time of day</li>
+                    <li>The air quality is fine</li>
                     <br><br><h2><a href='{get_location_link()}'>
                     Here's a close place for you to walk to.</a></h2>"""
 
@@ -115,36 +175,51 @@ if cab.get("weather", "alert_walk_sent") < (time.time() - 43200) and 10 <= now.h
         cab.put("weather", "alert_walk_sent", int(time.time()))
         cab.put("Walk Alert Sent")
 
-plantyAlertSent = cab.get("weather", "alert_planty_sent")
-plantyAlertChecked = cab.get("weather", "alert_planty_checked")
+planty_alert_sent = cab.get("weather", "alert_planty_sent")
+planty_alert_checked = cab.get("weather", "alert_planty_checked")
 
-if len(sys.argv) > 1 and sys.argv[1] == 'force' or \
-    (not plantyAlertSent or not plantyAlertChecked or
-     (int(plantyAlertSent) < (time.time() - 43200) and
-        int(plantyAlertChecked) < (time.time() - 21600))):
-    cab.log(
-        f"Checked Planty ({plantyStatus}): low {low_tomorrow}, high {high}")
+if (
+    len(sys.argv) > 1
+    and sys.argv[1] == "force"
+    or (
+        not planty_alert_sent
+        or not planty_alert_checked
+        or (
+            int(planty_alert_sent) < (time.time() - 43200)
+            and int(planty_alert_checked) < (time.time() - 21600)
+        )
+    )
+):
+    cab.log(f"Checked Planty ({planty_status}): low {low_tomorrow}, high {high}")
     cab.put("weather", "alert_planty_checked", int(time.time()))
-    if low_tomorrow < 55 and plantyStatus == "out":
+    
+    if low_tomorrow < 55 and planty_status == "out":
         try:
-            mail.send("Take Planty In This Afternoon",
-                      (f"Hi Tyler,<br><br>The low tonight is {low_tomorrow}°."
-                       f" Please take Planty in!"),
-                      to_addr=cab.get("weather", "alert_planty_emails"))
-            cab.put(
-                "weather", "alert_planty_sent", int(time.time()))
+            mail.send(
+                "Take Planty In This Afternoon",
+                (
+                    f"Hi Tyler,<br><br>The low tonight is {low_tomorrow}°."
+                    f" Please take Planty in!"
+                ),
+                to_addr=cab.get("weather", "alert_planty_emails"),
+            )
+            cab.put("weather", "alert_planty_sent", int(time.time()))
         except IOError as e:
             cab.log(f"Could not send Planty email: {e}", level="error")
         cab.put("planty", "status", "in")
-    if (high > 80 or low_tomorrow >= 56) and plantyStatus == "in":
+    if (high > 80 or low_tomorrow >= 56) and planty_status == "in":
         try:
-            EMAIL = (f"Hi Tyler,<br><br>It looks like a nice day!"
-                     f" It's going to be around {high}°. Please take Planty out.")
+            EMAIL = (
+                f"Hi Tyler,<br><br>It looks like a nice day!"
+                f" It's going to be around {high}°. Please take Planty out."
+            )
 
-            mail.send("Take Planty Out This Afternoon", EMAIL,
-                      to_addr=cab.get("weather", "alert_planty_emails"))
-            cab.put(
-                "weather", "alert_planty_sent", int(time.time()))
+            mail.send(
+                "Take Planty Out This Afternoon",
+                EMAIL,
+                to_addr=cab.get("weather", "alert_planty_emails"),
+            )
+            cab.put("weather", "alert_planty_sent", int(time.time()))
         except IOError as e:
             cab.log(f"Could not send Planty email: {e}", level="error")
         cab.put("planty", "status", "out")
