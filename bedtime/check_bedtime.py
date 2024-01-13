f89b421711caf6622e97a7383c709cba6610c5b5
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2024-01-12 21:36:29 -0800

    ANT-108: Added partial bedtime refund for early bedtimes

--- bedtime/check_bedtime.py
+++ bedtime/check_bedtime.py
@@ -1,8 +1,9 @@
 """
 Check Bedtime
 
-If my bedtime is later than allowed through
-cabinet -> bedtime -> limit, then send an email to myself
+If my bedtime is later than allowed through cab -> bedtime -> limit, then send an email to myself.
+If my bedtime is earlier than the limit, 
+    receive a refund of 50% of the penalty for the same time difference.
 """
 
 import csv
@@ -15,6 +16,17 @@ CAB = Cabinet()
 # Set the path to the log file
 LOG_FILE = "/home/tyler/syncthing/log/log_bedtime.csv"
 
+
+def calculate_time_difference(time1, time2):
+    """
+    Calculate the time difference in minutes between actual bedtime and limit.
+    """
+    return (
+        datetime.datetime.combine(datetime.date.today(), time1)
+        - datetime.datetime.combine(datetime.date.today(), time2)
+    ).seconds / 60
+
+
 with open(LOG_FILE, "r", encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile)
 
@@ -25,23 +37,43 @@ with open(LOG_FILE, "r", encoding="utf-8") as csvfile:
             BEDTIME = datetime.datetime.strptime(row[2], "%H:%M").time()
             break
 
-    # 24-hour format as a string, i.e. "1:30"
-    bedtime_limit = CAB.get("bedtime", "limit")
+    bedtime_limit_obj = CAB.get("bedtime", "limit")
+    bedtime_limit = bedtime_limit_obj.max_bedtime
+    max_penalty = bedtime_limit_obj.max_penalty
+    charity_balance = bedtime_limit_obj.charity_balance
 
     if bedtime_limit:
         bedtime_limit = datetime.datetime.strptime(bedtime_limit, "%H:%M").time()
 
-        if BEDTIME and (bedtime_limit <= BEDTIME <= datetime.time(6, 0)):
-            delta_minutes = (
-                datetime.datetime.combine(datetime.date.today(), BEDTIME)
-                - datetime.datetime.combine(datetime.date.today(), bedtime_limit)
-            ).seconds / 60
-            bedtime_limit_amount = min(int(delta_minutes), 30)  # limit to $30
+        if BEDTIME:
+            if bedtime_limit <= BEDTIME <= datetime.time(6, 0):
+                # Late bedtime penalty
+                delta_minutes = calculate_time_difference(BEDTIME, bedtime_limit)
+                penalty_amount = min(int(delta_minutes), max_penalty)
+                charity_balance += penalty_amount
+                MAIL.send(
+                    f"Late Bedtime: ${penalty_amount} to Charity",
+                    f"Your bedtime last night was {BEDTIME.strftime('%H:%M')}. \
+                                Please get to bed at a more reasonable time.",
+                )
+            elif BEDTIME < bedtime_limit:
+                # Early bedtime refund
+                delta_minutes = calculate_time_difference(bedtime_limit, BEDTIME)
 
-            MAIL.send(
-                f"Late Bedtime: ${bedtime_limit_amount} to Charity",
-                f"Your bedtime last night was {BEDTIME.strftime('%H:%M')} \
-                                - please get to bed at a more reasonable time.",
-            )
-        else:
+                # 50% refund, limit to 50% of max penalty
+                refund_amount = min(int(delta_minutes / 2), (max_penalty / 2))
+
+                charity_balance -= refund_amount
+                MAIL_SUBJ = f"Early Bedtime: ${refund_amount} Refund"
+
+                if charity_balance < 0:
+                    MAIL_SUBJ = "Early Bedtime: 50% Off Pushups for Today"
+
+                MAIL.send(
+                    MAIL_SUBJ,
+                    f"Thank you for going to bed early at {BEDTIME.strftime('%H:%M')}. \
+                                Your balance is ${charity_balance}.",
+                )
+
             CAB.log(f"Found bedtime last night: {BEDTIME}", level="info")
+            CAB.put("bedtime", "limit", "charity_balance", max(charity_balance, 0))
--- dailystatus/main.py
+++ dailystatus/main.py
@@ -25,6 +25,7 @@ import pwd
 import datetime
 import glob
 import subprocess
+import json
 
 from remind import remindmail_utils
 import cabinet
@@ -40,18 +41,20 @@ TODAY = datetime.date.today()
 PATH_BACKEND = cab.get("path", "cabinet", "log-backup")
 PATH_LOG_BACKEND = os.path.join(PATH_BACKEND, "log")
 PATH_BASHRC = os.path.join(f"/home/{DIR_USER}/.bashrc")
-PATH_NOTES = cab.get('path', 'notes')
+PATH_NOTES = cab.get("path", "notes")
 PATH_LOG_TODAY = os.path.join(cab.path_log, str(TODAY))
 BEDTIME_LIMIT = cab.get("bedtime", "limit")
 
 LOG_BACKUPS_MAX = cab.get("backups", "log_backup_limit") or 14
-LOG_BACKUPS_LOCATION = os.path.join(cab.get('path', 'backups'), "log")
+LOG_BACKUPS_LOCATION = os.path.join(cab.get("path", "backups"), "log")
 
 # get reminders sent
 REMINDERS_COUNT = remindmail_utils.RemindMailUtils().get_sent_today() or -1
 
 # log reminders
-with open(os.path.join(PATH_LOG_BACKEND, "log_reminders.csv"), "a+", encoding="utf-8") as file_rmm:
+with open(
+    os.path.join(PATH_LOG_BACKEND, "log_reminders.csv"), "a+", encoding="utf-8"
+) as file_rmm:
     file_rmm.write(f"\n{TODAY},{REMINDERS_COUNT}")
 
 
@@ -70,7 +73,7 @@ backup_commands = [
     f"cp -r {PATH_BASHRC} '{os.path.join(PATH_LOG_BACKEND, 'bash', f'Bash {TODAY}.md')}'",
     f"zip -r '{os.path.join(PATH_LOG_BACKEND, 'notes', f'notes {TODAY}.zip')}' {PATH_NOTES}",
     f"zip -r '{os.path.join(LOG_BACKUPS_LOCATION, f'log folder backup {TODAY}.zip')}' "
-    f"{PATH_LOG_BACKEND} --exclude='{os.path.join(PATH_LOG_BACKEND, 'songs', '*')}'"
+    f"{PATH_LOG_BACKEND} --exclude='{os.path.join(PATH_LOG_BACKEND, 'songs', '*')}'",
 ]
 
 try:
@@ -91,50 +94,62 @@ if excess_count > 0:
 
 cab.log(f"Cron, Bash, Notes, and remind.md copied to {PATH_LOG_BACKEND}.")
 
-# publish bedtime limit
-try:
-    with open(cab.path_cabinet + "/keys/BEDTIME_LIMIT", 'w', encoding="utf8") as file_bedtime_limit:
-        file_bedtime_limit.write(BEDTIME_LIMIT)
-except Exception as error:
-    cab.log(f"Error updating Bedtime Limit file: {str(error)}", level="error")
+# publish bedtime limit to be read by tools/bedtime/post.php
+bedtime_output = json.dumps(cab.get("bedtime", "limit"))
+BEDTIME_KEY = "/home/tyler/syncthing/cabinet/keys/BEDTIME"
 
+# Write the JSON string to the file
+try:
+    with open(BEDTIME_KEY, "w", encoding="utf-8") as file:
+        file.write(bedtime_output)
+except Exception as error:
+    cab.log(f"Could not write bedtime key: {error}", level="error")
 
 # spotify stats
 spotify_count = cab.get("spotipy", "total_tracks")
 spotify_avg_year = cab.get("spotipy", "average_year")
 SPOTIFY_STATS = "<b>Spotify Stats:</b><br>"
 
-spotify_log_array = cab.get_file_as_array(
-    "LOG_SPOTIFY.log", file_path=PATH_LOG_TODAY)
+spotify_log_array = cab.get_file_as_array("LOG_SPOTIFY.log", file_path=PATH_LOG_TODAY)
 
 if spotify_log_array is not None:
-    SPOTIFY_LOG = "<font face='monospace'>" + \
-        '<br>'.join(cab.get_file_as_array(
-            "LOG_SPOTIFY.log", file_path=PATH_LOG_TODAY)) + "</font><br><br>"
+    SPOTIFY_LOG = (
+        "<font face='monospace'>"
+        + "<br>".join(
+            cab.get_file_as_array("LOG_SPOTIFY.log", file_path=PATH_LOG_TODAY)
+        )
+        + "</font><br><br>"
+    )
 
     if "ERROR —" in SPOTIFY_LOG:
-        status_email_alerts.append('Spotify')
+        status_email_alerts.append("Spotify")
         SPOTIFY_STATS += "Please review your songs! We found some errors.<br><br>"
 
 SPOTIFY_STATS += f"""
     You have {spotify_count} songs; the mean song is from {spotify_avg_year}.<br><br>
     """
 
-if 'Spotify' in status_email_alerts and spotify_log_array is not None:
+if "Spotify" in status_email_alerts and spotify_log_array is not None:
     SPOTIFY_STATS += SPOTIFY_LOG
 
 # daily log
 daily_log_file_array = cab.get_file_as_array(
-    f"LOG_DAILY_{TODAY}.log", file_path=PATH_LOG_TODAY)
-DAILY_LOG_FILE = '<br>'.join(daily_log_file_array)
+    f"LOG_DAILY_{TODAY}.log", file_path=PATH_LOG_TODAY
+)
+DAILY_LOG_FILE = "<br>".join(daily_log_file_array)
 
 if "ERROR —" in DAILY_LOG_FILE or "CRITICAL —" in DAILY_LOG_FILE:
     status_email_alerts.append("Errors")
 if "WARNING —" in DAILY_LOG_FILE:
     status_email_alerts.append("Warnings")
 
-DAILY_LOG_FILTERED = '<br>'.join([item for item in daily_log_file_array if (
-    "ERROR" in item or "WARN" in item or "CRITICAL" in item)])
+DAILY_LOG_FILTERED = "<br>".join(
+    [
+        item
+        for item in daily_log_file_array
+        if ("ERROR" in item or "WARN" in item or "CRITICAL" in item)
+    ]
+)
 
 if len(DAILY_LOG_FILTERED) > 0:
     STATUS_EMAIL += f"""
@@ -147,7 +162,9 @@ if len(DAILY_LOG_FILTERED) > 0:
 STATUS_EMAIL += SPOTIFY_STATS
 
 SENT_TODAY = " was" if REMINDERS_COUNT == 1 else "s were"
-STATUS_EMAIL += f"<b>Reminders:</b><br>{REMINDERS_COUNT} reminder{SENT_TODAY} sent today.<br><br>"
+STATUS_EMAIL += (
+    f"<b>Reminders:</b><br>{REMINDERS_COUNT} reminder{SENT_TODAY} sent today.<br><br>"
+)
 
 # weather
 weather_data = cab.get("weather", "data")
@@ -168,8 +185,10 @@ STATUS_EMAIL += WEATHER_DATA_TEXT
 
 STATUS_EMAIL = STATUS_EMAIL.replace("<br><br><br><br>", "<br><br>")
 
-STATUS_EMAIL_WARNINGS_TEXT = "- Check " + \
-    ', '.join(status_email_alerts) + \
-    " " if len(status_email_alerts) > 0 else ""
+STATUS_EMAIL_WARNINGS_TEXT = (
+    "- Check " + ", ".join(status_email_alerts) + " "
+    if len(status_email_alerts) > 0
+    else ""
+)
 
 mail.send(f"Daily Status {STATUS_EMAIL_WARNINGS_TEXT}- {TODAY}", STATUS_EMAIL)
