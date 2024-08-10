a638ac3858f36c3ac356e092717073d0f2658527
Tyler Woodfin <14207553+tylerjwoodfin@users.noreply.github.com>
2024-08-09 21:30:20 -0700

    ANT-138: Refactored one-more-hour to use 'at'

--- pihole/one_more_hour.py
+++ pihole/one_more_hour.py
@@ -5,10 +5,8 @@ in Pihole. After 1 hour, it reintroduces the block.
 """
 
 import subprocess
-import time
 from datetime import datetime, timedelta
 from cabinet import Cabinet
-from apscheduler.schedulers.background import BackgroundScheduler
 
 cabinet = Cabinet()
 times_used = cabinet.get("pihole", "times_unblocked") or 0
@@ -24,41 +22,29 @@ def execute_command(command):
 
 def schedule_commands():
     """
-    Schedule commands using APScheduler. This sets up two commands:
+    Schedule commands using the `at` command. This sets up two commands:
     one to execute immediately and another to execute one hour later.
     """
-    # Create a scheduler in the background
-    scheduler = BackgroundScheduler()
+    path_script = "/home/tyler/git/tools/pihole/downtime.sh"
+    cmd_unblock = f"zsh {path_script} allow afternoon"
+    cmd_reblock = f"zsh {path_script} block afternoon"
 
-    # Schedule the first command to run immediately
-    scheduler.add_job(execute_command,
-                      args=["bash /home/tyler/git/tools/pihole/downtime.sh allow afternoon"],
-                      trigger='date')
+    # unblock immediately
+    execute_command(cmd_unblock)
 
-    # Schedule the second command to run 1 hour later
-    scheduler.add_job(execute_command,
-                      args=["bash -m /home/tyler/git/tools/pihole/downtime.sh block afternoon"],
-                      trigger='date', run_date=datetime.now() + timedelta(hours=1))
+    # schedule re-block in 1 hour
+    reblock_time = datetime.now() + timedelta(hours=1)
+    at_command = f"echo '{cmd_reblock}' | at {reblock_time.strftime('%H:%M')}"
+
+    # Schedule the re-block command
+    subprocess.run(at_command, shell=True, check=True)
 
     # Log Times Used
-    cabinet.put("pihole", "times_unblocked", times_used + 1) # type: ignore
+    cabinet.put("pihole", "times_unblocked", times_used + 1)
     cabinet.update_cache()
 
     print(f"Fine, unblocking, but you've used this {times_used} times before.")
     print("Sleep is important!")
-
-    # Start the scheduler
-    scheduler.start()
-
-    # Keep the script running to allow jobs to execute
-    try:
-        while True:
-            time.sleep(2)
-    except (KeyboardInterrupt, SystemExit):
-        # Shutdown the scheduler when exiting the app
-        cabinet.log("Re-blocking")
-        execute_command("bash /home/tyler/git/tools/pihole/downtime.sh block afternoon")
-        scheduler.shutdown()
 
 if __name__ == '__main__':
     schedule_commands()
